0fd99895995f103f7c16dd4ae1ace13e4d249727
musfikur-rahman <musfik.rizvi@gmail.com>
2022-06-24 21:51:45 +0600

    t16

--- EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
+++ EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
@@ -19,7 +19,6 @@ namespace EmployeeManagement.ViewModels
         public string Email { get; set; }
         [Required]
         public Dept? Department { get; set; }
-
         public IFormFile Photo { get; set; }
     }
 }
